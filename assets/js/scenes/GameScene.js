61eb8b3452272dfa5c6c6539d64a0d1da95193ce
Matt Petersen <mattp0510@gmail.com>
2022-03-15 17:56:35 -0500

    updated player animations

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -1,107 +1,129 @@
 //create a scene
 let gameScene = new Phaser.Scene('Game');
 
-gameScene.init = function(){
-
+gameScene.init = function() {
+	this.mainPlayerSpeed = 150;
 };
 
 //load in assets
-gameScene.preload = function(){
-    //load images (label, location)
-    this.load.image('baseTiles', 'assets/images/background.png');               //tiles
-    this.load.tilemapTiledJSON('map', 'assets/images/WorldMap1.json');          //json from Tiled
-    this.load.image('player', 'assets/images/player.png');                      //Player
-    this.load.image('enemy', 'assets/images/dragon.png');                       //Enemy
+gameScene.preload = function() {
+	//load images (label, location)
+	this.load.image('baseTiles', 'assets/images/background.png'); //tiles
+	this.load.tilemapTiledJSON('map', 'assets/images/WorldMap1.json'); //json from Tiled
+	this.load.image('enemy', 'assets/images/dragon.png'); //Enemy
+	this.load.spritesheet('mainPlayer', 'assets/images/Character/mainPlayer.png', { frameWidth: 24, frameHeight: 24 });
 };
 
-gameScene.create = function(){
-    //add in tiled map, tileset, layers
-    this.map = this.make.tilemap({ key: 'map'});                                       //create map: key matches name given in preload
-    const tileset = this.map.addTilesetImage('background', 'baseTiles', 32, 32);             //add tilset image: name of tileset used in tiled, key of image in preload, tilewidth, tileheight
-    const level = this.map.createStaticLayer('Tile Layer 1', tileset, 0, 0);                 //name of tile layer, tileset used, x&y position
-    this.water = this.map.createStaticLayer('water', tileset, 0, 0); 
-    this.map.x = this.map.displayWidth;
-    this.map.y = this.map.displayHeight;
-    this.water.setCollisionByExclusion([-1]);
-    
+gameScene.create = function() {
+	//add in tiled map, tileset, layers
+	this.map = this.make.tilemap({ key: 'map' }); //create map: key matches name given in preload
+	const tileset = this.map.addTilesetImage('background', 'baseTiles', 32, 32); //add tilset image: name of tileset used in tiled, key of image in preload, tilewidth, tileheight
+	const level = this.map.createStaticLayer('Tile Layer 1', tileset, 0, 0); //name of tile layer, tileset used, x&y position
+	this.water = this.map.createStaticLayer('water', tileset, 0, 0);
+	this.map.x = this.map.displayWidth;
+	this.map.y = this.map.displayHeight;
+	this.water.setCollisionByExclusion([ -1 ]);
 
-    //prevents player from moving off map
-    this.physics.world.bounds.width = this.map.widthInPixles;
-    this.physics.world.bounds.height = this.map.heightInPixles;
-    
-    //player sprite -- add with physics to allow input from keyboard
-    this.player = this.physics.add.sprite(0, 1750, 'player');
+	//prevents player from moving off map
+	this.physics.world.bounds.width = this.map.widthInPixles;
+	this.physics.world.bounds.height = this.map.heightInPixles;
 
-    //scale player sprite down
-    this.player.setScale(.1);
-    //sets player on top of background
-    this.player.depth = 1;
+	//player sprite -- add with physics to allow input from keyboard
+	this.mainPlayer = this.physics.add.sprite(150, 1750, 'mainPlayer');
 
+	//this.mainPlayer = gameScene.add.sprite(150, 1750, 'mainPlayer');
+	this.mainPlayer.setScale(1.2);
 
+	//mainPlayer walking animation
+	this.anims.create({
+		key: 'walk',
+		frames: this.anims.generateFrameNames('mainPlayer', { frames: [ 1, 2, 3, 4, 5, 6 ] }),
+		frameRate: 12,
+		yoyo: true,
+		repeat: -1
+	});
 
-    //enemy sprite
-    this.enemy = this.add.sprite(500, 1750, 'enemy');
-    this.enemy.setScale(1);
+	//sets player on top of background
+	this.mainPlayer.depth = 1;
 
-    //rotate sprite
-    this.enemy.flipX = true;
+	//enemy sprite
+	this.enemy = this.add.sprite(500, 1750, 'enemy');
+	this.enemy.setScale(1);
 
-    //set edges of world
-    this.player.setCollideWorldBounds(true);
+	//rotate sprite
+	this.enemy.flipX = true;
 
-    //prevent player from access blocked areas (ex. water)
-    this.physics.add.collider(this.player, this.water);
+	//set edges of world
+	this.mainPlayer.setCollideWorldBounds(true);
 
-    this.cursors = this.input.keyboard.createCursorKeys();
+	//prevent player from access blocked areas (ex. water)
+	this.physics.add.collider(this.mainPlayer, this.water);
+
+	this.cursors = this.input.keyboard.createCursorKeys();
 };
 
-gameScene.update = function(){
-    //stops player from moving
-    this.player.setVelocityY(0);
-    this.player.setVelocityX(0);
+gameScene.update = function() {
+	//stops player from moving
+	this.mainPlayer.setVelocityX(0);
+	this.mainPlayer.setVelocityY(0);
 
-    //checks if key is pressed and moves player on that axis
-    if(this.cursors.up.isDown==true){
-        this.player.setVelocityY(-100);
-    }
-    if(this.cursors.down.isDown==true){
-        this.player.setVelocityY(100);
-    }
-    if(this.cursors.right.isDown==true){
-        this.player.setVelocityX(100);
-    }
-    if(this.cursors.left.isDown==true){
-        this.player.setVelocityX(-100);
-    }
-    
-    //sets camera to follow player
-    this.cameras.main.centerOn(this.player.x, this.player.y);
-    this.cameras.main.setBounds(0, 0, this.map.widthInPixles, this.map.heightInPixles);
+	//checks if key is pressed and moves player on that axis
+	if (this.cursors.up.isDown == true) {
+		this.mainPlayer.setVelocityY(-this.mainPlayerSpeed);
+		if (!this.mainPlayer.anims.isPlaying) {
+			this.mainPlayer.play('walk');
+		}
+	} else if (this.cursors.down.isDown == true) {
+		this.mainPlayer.setVelocityY(this.mainPlayerSpeed);
+		if (!this.mainPlayer.anims.isPlaying) {
+			this.mainPlayer.play('walk');
+		}
+	} else if (this.cursors.right.isDown == true) {
+		this.mainPlayer.setVelocityX(this.mainPlayerSpeed);
 
-    //get player and enemy area
-    let playerArea = this.player.getBounds();
-    let enemyArea = this.enemy.getBounds();
+		this.mainPlayer.flipX = false;
+		if (!this.mainPlayer.anims.isPlaying) {
+			this.mainPlayer.play('walk');
+		}
+	} else if (this.cursors.left.isDown == true) {
+		this.mainPlayer.setVelocityX(-this.mainPlayerSpeed);
 
-    if(Phaser.Geom.Intersects.RectangleToRectangle(playerArea, enemyArea)){
-        //restart game if player overlaps enemy
-        this.scene.restart();
-    }
-    
+		this.mainPlayer.flipX = true;
+		if (!this.mainPlayer.anims.isPlaying) {
+			this.mainPlayer.play('walk');
+		}
+	} else {
+		this.mainPlayer.anims.stop('walk');
+		this.mainPlayer.setFrame(0);
+	}
+
+	//sets camera to follow player
+	this.cameras.main.centerOn(this.mainPlayer.x, this.mainPlayer.y);
+	this.cameras.main.setBounds(0, 0, this.map.widthInPixles, this.map.heightInPixles);
+
+	//get player and enemy area
+	let playerArea = this.mainPlayer.getBounds();
+	let enemyArea = this.enemy.getBounds();
+
+	if (Phaser.Geom.Intersects.RectangleToRectangle(playerArea, enemyArea)) {
+		//restart game if player overlaps enemy
+		this.scene.restart();
+	}
 };
 
 let config = {
-    type: Phaser.AUTO,          // lets phaser decide whether or not to use WebGL or Canvas
-    width: 640,                 //size of game
-    height: 360,
-    scene: gameScene,            //this is where you pass in which scene to use
-    physics:{
-        default:"arcade",
-        arcade:{
-            debug:true
-        }
-    },
-    pixelArt: true,
-    roundPixels: true,
+	type: Phaser.AUTO, // lets phaser decide whether or not to use WebGL or Canvas
+	width: 640, //size of game
+	height: 360,
+	scene: gameScene, //this is where you pass in which scene to use
+	physics: {
+		default: 'arcade',
+		arcade: {
+			debug: true
+		}
+	},
+	pixelArt: true,
+	roundPixels: true
 };
 
 // create the game and pass in the configuration
